71245d66bb2b2d6895c83b0560665bfc67f69de9
juan512 <juandanielpcutile.jc@gmail.com>
2022-08-24 22:09:16 -0400

    Login Problems

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,4 +1,29 @@
 const { errorMessages } = require('../constants/messages');
+const AuthClient = require('../clients/keycloakClient');
+
+exports.validToken = async (req, res, next) => {
+
+  try {
+    const { headers } = req;
+    const { authorization } = headers;
+    const { acces_token, refresh_token } = authorization;
+    console.log(authorization);
+    console.log(JSON.stringify(authorization));
+    const info = await AuthClient.getInfo(token)
+    const status = info.response.status;
+    if (status == 401) {
+      return res.status(401).json({
+        message: "Invalid User"
+      });
+    } else {
+      next(); 
+    }     
+  } catch (error) {
+    return res.status(422).json({
+      message: "error"
+    });
+  }
+}
 
 exports.validProductData = (req, res, next) => {
   try {
